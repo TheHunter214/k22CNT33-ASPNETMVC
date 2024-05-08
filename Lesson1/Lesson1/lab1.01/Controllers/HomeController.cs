bd6c1d3c0a7c29d61687c30a3313a40f54c80214
TheHunter214 <minhnamtmn@gmail.com>
2024-05-08 21:09:36 +0700

    Lesson01

---
+++ Lesson1/Lesson1/lab1.01/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab1._01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            // su dung ViewBag de dua du lieu ra View 
+            ViewBag.message = "Chao mung ban da den Protocol";
+            return View();
+        }
+        public ActionResult About()
+        {
+            //su dung ViewBag de dua du lieu ra View
+            ViewBag.message = "Day la trang chuong trinh Protocol";
+            return View();
+        }
+    }
+}
